95f6c20f30648c39d93f8f8c358f6936424ea976
milesmcbain <miles.mcbain@gmail.com>
2018-03-01 23:17:33 +1000

    Added carbon support.

--- R/gistfo.R
+++ R/gistfo.R
@@ -1,9 +1,30 @@
+CARBON_URL <- "https://carbon.now.sh/?bg=rgba(0,0,0,0)&t=solarized%20dark&l=r&ds=true&wc=true&wa=true&pv=48px&ph=32px&ln=true"
+
 #' Create a private Github gist containing the active RStudio tab and browse to it.
 #' File is named: RStudio_<project>_<filename or file_id>. file_id is a unique id for
 #' untitled files. It does not relate to the untitled number.
 #' @return nothing.
 #' @export
-gistfo <- function(){
+gistfo <- function() gistfo_base(mode = "gistfo")
+
+#' Create a public Github gist containing the active RStudio tab and browse to it,
+#' then open the gist in carbon.now.sh, for easily Tweeting a saucy code pic.
+#' Gist file is named: RStudio_<project>_<filename or file_id>. file_id is a unique id for
+#' untitled files. It does not relate to the untitled number.
+#' @return nothing.
+#' @export
+gistfoc <- function() gistfo_base(mode = "carbon")
+
+gistfo_base <- function(mode){
+        if(mode == "gistfo"){
+          browse <- TRUE
+          public <- FALSE
+        } else if(mode == "carbon"){
+          browse <- TRUE
+          public <- TRUE
+        } else{
+          stop("mode must be 'gistfo' or 'carbon'")
+        }
         source_context <- rstudioapi::getSourceEditorContext()
         if(source_context$path == ''){
           name <- paste("unititled",source_context$id,sep = "_")
@@ -18,10 +39,14 @@ gistfo <- function(){
         }
         gist_name <- paste("RStudio",project,name, sep="_")
 
-        gistr::gist_create(filename = gist_name,
-                           public = FALSE,
-                           browse = TRUE,
+        the_gist <- gistr::gist_create(filename = gist_name,
+                           public = public,
+                           browse = browse,
                            code = source_context$contents
                            )
+        if(mode == "carbon"){
+          browseURL(paste0(CARBON_URL, the_gist$url))
+          clipr::write_clip(the_gist$url)
+        }
         invisible(NULL)
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Get It Somewhere The F*** Online 
 Turn your RStudio untitled tabs into gists. You monsters.
+
+Carbon Edition Now Available:
+![]()
